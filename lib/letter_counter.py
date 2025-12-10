4764b763e1358e28741a504270ea29c2d584b03f
Jonathan-Smith9410 <jsmith300599@googlemail.com>
2025-12-10 14:03:49 +0000

    Completed

--- lib/letter_counter.py
+++ lib/letter_counter.py
@@ -1,5 +1,24 @@
 # File: lib/letter_counter.py
 
+# Original bugged code:
+
+# class LetterCounter:
+#     def __init__(self, text):
+#         self.text = text
+
+#     def calculate_most_common(self):
+#         counter = {}
+#         most_common = None
+#         most_common_count = 1
+#         for char in self.text:
+#             if not char.isalpha():
+#                 continue
+#             counter[char] = counter.get(char, 1) + 1
+#             if counter[char] > most_common_count:
+#                 most_common = char
+#                 most_common_count += counter[char]
+#         return [most_common_count, most_common]
+
 class LetterCounter:
     def __init__(self, text):
         self.text = text
@@ -7,14 +26,14 @@ class LetterCounter:
     def calculate_most_common(self):
         counter = {}
         most_common = None
-        most_common_count = 1
+        most_common_count = 0 # Changed from 1 to 0 as would always be 1 ahead of actual value due to loop incrementing value by 1. Not actually necesary to change - see line 36 annotation.
         for char in self.text:
             if not char.isalpha():
                 continue
-            counter[char] = counter.get(char, 1) + 1
-            if counter[char] > most_common_count:
+            counter[char] = counter.get(char, 0) + 1 # Changed (char, 1) to (char 0) or it initiated every new char with 2 instead of 1
+            if counter[char] > most_common_count: 
                 most_common = char
-                most_common_count += counter[char]
+                most_common_count = counter[char] # was originally += counter += car which just added on to original value. This gets the value from the dict.
         return [most_common_count, most_common]
 
 
